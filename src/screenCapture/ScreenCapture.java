c46101cd77ef56a290849590140ae2c9e963e111
badar <badarshahzad54@gmail.com>
2017-03-19 15:58:44 +0500

    Screen capture code

---
+++ src/screenCapture/ScreenCapture.java
@@ -0,0 +1,32 @@
+package screenCapture;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class ScreenCapture {
+	 public static void main(String[] args) {
+	        try {
+	            Robot robot = new Robot();
+	            String format = "jpg";
+	            String fileName = "PartialScreenshot." + format;
+	             
+	            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	            Rectangle captureRect = new Rectangle(0, 0, screenSize.width / 2, screenSize.height / 2);
+	            BufferedImage screenFullImage = robot.createScreenCapture(captureRect);
+	            ImageIO.write(screenFullImage, format, new File(fileName));
+	             
+	            System.out.println("A partial screenshot saved!");
+	        } catch (AWTException | IOException ex) {
+	            System.err.println(ex);
+	        }
+	    }
+}
