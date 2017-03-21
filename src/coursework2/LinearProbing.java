48d55e701420f0f05e517cb4ce0f7554914b9393
badar <badarshahzad54@gmail.com>
2017-03-21 19:27:44 +0500

    Coursework 2 completed Now its open source

--- src/coursework2/LinearProbing.java
+++ src/coursework2/LinearProbing.java
@@ -13,40 +13,38 @@ import java.util.Scanner;
 public class LinearProbing {
 
 	public static int steps = 0;
+	public static int hashKey;
+	public static int bukketLength;
+	public static Writer writer = null;
 
-	static int hashKey;
-	static int bukketLength;
 	public static int hashKeyFunction(int key) {
 		hashKey = key % bukketLength;
 		return hashKey;
 	}
 
-	
-	public static void insert(int buket[], int keyIndex, int num) throws IOException {
+	public static void insert(int buket[], int num) throws IOException {
 
-		if (buket[keyIndex] == 0) {
+		if (buket[hashKey] == 0) {
 
-			writer.write("\t I" + num + "@" + keyIndex + "\t");
-			buket[keyIndex] = num;
+			writer.write(" I" + num + "@" + hashKey + " ");
+			buket[hashKey] = num;
 			steps++;
 
 		} else {
 
-			keyIndex++;
+			hashKey++;
 
-			if (keyIndex == 101) {
-				keyIndex = 0;
+			if (hashKey == 101) {
+				hashKey = 0;
 			}
 
-			writer.write("\t P" + keyIndex + "\t");
+			writer.write(" P" + hashKey + " ");
 			steps++;
-			insert(buket, keyIndex, num);
+			insert(buket, num);
 
 		}
 
 	}
-
-	static Writer writer = null;
 
 	public static void main(String args[]) throws IOException {
 
@@ -58,107 +56,102 @@ public class LinearProbing {
 
 		try {
 
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Results.txt"), "UTF-8"));
-			writer.write("\t14031264\n\n");
-			writer.write("\tHash Table Insertion Linear Probing ");
+			// --------Here I am just write the details about data structure and
 
-			int k = 10;
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Reslut.txt"), "UTF-8"));
+			writer.write(" 4031264\n\n");
+			writer.write(" Hash Table Insertion Linear Probing ");
+
+			// Initially i am starting from 10 then I have added condition when
+			// k==9 the k will become k=100 and
+			// when k==99 then k will initazlize to k==1000
+
 			int count = 0;
+			int k = 10;
 			for (int ak = 0; ak < k; ak++) {
-
+				// Here every iteration complete like from 10 times when 1 iteration complete and steps count and
+				// and inserted into buket then for step 2 again here am just initializing again array to same buket
+				// I did this to escape from to intialize with zero with loop
+				// I giving array lenght to below globel variable bukketlength
+				
 				buket = new int[101];
 				bukketLength = buket.length;
 
-				if (k == 10) {
-					writer.write("\n \t 1) K = 10 \n");
+				// these are just lines that will print in file
+				if (k == 10 && ak == 0) {
+					writer.write("\n  1) K = 10 \n");
 				}
-				if (k == 100) {
-					writer.write("\t 2) K = 100 \n");
+				if (k == 100 && ak == 1) {
+
+					writer.write("\n  2) K = 100 \n");
 				}
-				if (k == 1000) {
-					writer.write("\t 3) K = 1000 \n");
+				if (k == 1000 && ak == 1) {
+					writer.write("\n  3) K = 1000 \n");
+
 				}
 
-				for (int a = 0; a < buket.length; a++) {
+				// Randome number genearating but N+1 as to include my uob number the random goes to N-1 not to N
+				// Hashey funciton is just return the key
 
-					num = random.nextInt(N);
-					//System.out.println(num);
+				for (int a = 0; a < 100; a++) {
+					
+					//Randome number assigning 
+					num = random.nextInt(N + 1);
+					//num = 14031259;
 					hashKey = hashKeyFunction(num);
 					steps++;
 
-					if (a % 3 == 0) {
-						
+					// The purpose of this line is to just add new line in file
+					// as printing on single line consequence of
+					// eclipse one line printing bug that still not resolve
+
+					if (a % 5 == 0) {
 						writer.write("\n ");
-						
 					}
 
-					writer.write("\t P" + hashKey + "\t");
-					insert(buket, hashKey, num);
-					a++;
-					
+					// before inserting key in buket I insert into file here
+					writer.write("  P" + hashKey);
+					insert(buket, num);
+
 				}
-				//System.out.println("Steps: " + steps);
-				writer.write("\n\n\n \t Number of Steps: S = " + steps + "\n");
+
+				// printing steps after each iterationg and counter is just to
+				// counting the total steps
+				// the purpose of steps=0 is to just starting again itration
+				// should be zero
+
+				writer.write("\n\n  Number of Steps: S = " + steps + "\n");
 				counter = counter + steps;
-				//System.out.println("Total count: " + counter);
-				//System.out.println("Ak count: \t" + ak);
 				steps = 0;
 
 				// A condition to convert 10 loop to hundred
+				// No confusion here as I am starting k again from k=0
 
 				if (ak == 9 && count == 0) {
 
-					writer.write("\n\t Average:" + counter + "/" + k + "=" + counter / k + "\n");
-					//System.out.println("\n\n \t Average: " + counter / 10 + "\n\n");
+					writer.write("\n\t  Average:" + counter + "/" + k + "=" + counter / k + "\n");
 					counter = 0;
 					k = 100;
-					ak = 0;
-					//System.out.println("\t counter: " + counter + "\tk is :" + k + "\tak:" + ak);
+					ak = -1;
 					count++;
-					
+
 				}
 
 				if (ak == 99 && count == 1) {
 
 					writer.write("\n\t Average:" + counter + "/" + k + "=" + counter / k + "\n");
-					//System.out.println("\n\n \t Average: " + counter / 100 + "\n\n");
 					counter = 0;
 					k = 1000;
-					ak = 0;
-					//System.out.println("\t counter: " + counter + "\tk is :" + k + "\tak:" + ak);
+					ak = -1;
 					count++;
-					
+
 				}
 
 				if (ak == 999 && count == 2) {
-					
-					writer.write("\n\t Average:" + counter + "/" + k + "=" + counter / k + "\n");
-					
-				}
-				
-				// -----------Testing---------------
-				/*
-				Scanner input = new Scanner(System.in);
-				System.out.println("Enter num: ");
-				num = random.nextInt(14031264);
-				int hashKey = 0;
-				int a = 0;
-				System.out.println("Frist step: " + steps);
-				while (a < 3) {
-					System.out.println("Frist step: " + steps);
-					System.out.println("Numer: " + num);
-					num = input.nextInt();
-					hashKey = hashKeyFunction(num, store.length);
-					steps++;
-					add(store, hashKey, num);
-					writer.write("\t P" + hashKey);
-					hash(store, key, num);
-					a++;
-				}
-				System.out.println("totoal" + steps);
-				 */			 
-				/// ----------end test
 
+					writer.write("\n\t Average:" + counter + "/" + k + "=" + counter / k + "\n");
+
+				}
 			}
 
 		} catch (UnsupportedEncodingException e) {
@@ -175,4 +168,5 @@ public class LinearProbing {
 			}
 		}
 	}
+
 }
---
+++ src/coursework2/test.java
@@ -0,0 +1,15 @@
+package coursework2;
+
+public class test {
+	
+	public static void main(String args []){
+		int [] kaka = new int[10];
+		int a=0;
+		while(a<kaka.length){
+		System.out.println(kaka[a]);
+		a++;
+		}
+		
+	}
+
+}
